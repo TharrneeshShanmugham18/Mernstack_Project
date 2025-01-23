9d6f71ec7da5589927679eccf21c6b8a6177575a
TharrneeshShanmugham18 <153155989+TharrneeshShanmugham18@users.noreply.github.com>
2025-01-23 20:50:16 +0530

    Add files via upload

---
+++ Day 8(22.01.2025)/components/About.jsx
@@ -0,0 +1,21 @@
+const About = () => {
+    return (
+        <div className="about-design">
+            <h1 className="about-title"></h1>
+            <p className="about-description">
+                This is a beautiful page.
+                <strong>Hydrogen is a great element</strong>
+                To create a good day out of everyone present.
+            </p>
+            <div className="image">
+                <img src="https://www.themoviedb.org/t/p/original/cZ3qu8mLLTSGOV6BX3dj9tDc4UB.jpg"></img>
+            </div>
+            <div className="image">
+                <img src="https://www.themoviedb.org/t/p/original/cZ3qu8mLLTSGOV6BX3dj9tDc4UB.jpg" alt="Nothing Here"></img>
+            </div>                
+        </div>
+    )
+};
+
+
+export default About;
